ce033019f420d2924d7e714557e7b892b9f06976
CalebCouch <calebblackcat@gmail.com>
2025-06-03 20:40:37 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 use pelican_ui::*;
-use pelican_ui::drawable::{Drawable, Shape, ShapeType, Color};
+use pelican_ui::drawable::{Component, Drawable, Shape, ShapeType, Color};
+use pelican_ui::layout::{DefaultStack, SizeRequest, Area, Layout};
+use pelican_ui::events::OnEvent;
+
+
+#[derive(Component, Debug)]
+pub struct FancyRect(DefaultStack, Shape, Shape);
+impl OnEvent for FancyRect {}
+
 
 #[derive(Clone)]
 struct App;
 
 impl Application for App {
     async fn new(_ctx: &mut Context) -> Box<dyn Drawable> {
-        Box::new(Shape{
-            shape: ShapeType::Rectangle(20.0, (200.0, 200.0)),
-            color: Color(255, 0, 0, 255)
-        })
+        Box::new(
+
+            FancyRect(, 
+                Shape{
+                    shape: ShapeType::Ellipse(10.0, (200.0, 200.0)),
+                    color: Color(255, 255, 0, 255)
+                },
+                Shape{
+                    shape: ShapeType::Rectangle(20.0, (200.0, 200.0)),
+                    color: Color(255, 0, 0, 255)
+                },
+            )
+        )
     }
 }
 impl Services for App {}
